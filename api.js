a3079a590800aaadcb70d3548d92bd45f9bacc20
Nassim <nassimelalam@gmail.com>
2019-10-05 13:50:50 +0200

    api

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 let api = axios.create({
-  // baseURL: "https://forever-express-mysql.herokuapp.com/"
-  baseURL: "http://localhost:8000/"
+  baseURL: "https://forever-express-mysql.herokuapp.com/"
+  //baseURL: "http://localhost:8000/"
 });
 
 export default api;
